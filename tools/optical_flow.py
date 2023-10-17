247834b2eb1e6b59dddb9c9b559c9dc8632f9c42
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2023-10-17 19:09:29 +0900

    modify

--- tools/optical_flow.py
+++ tools/optical_flow.py
@@ -15,10 +15,13 @@ def parser():
         type=str,
         help="path of input dataset directory",
     )
+    # optional
     parser.add_argument(
-        "dataset_type",
+        "--dataset_type",
         type=str,
-        help="select from 'collective' or 'volleyball'",
+        required=False,
+        default=None,
+        help="select from 'collective' or 'volleyball or 'video', default by None.",
     )
 
     return parser.parse_args()
@@ -29,6 +32,12 @@ def main():
     dataset_dir = args.dataset_dir
     dataset_type = args.dataset_type.lower()
 
+    if dataset_type is None:
+        if dataset_dir.endswith("/"):
+            dataset_type = os.path.basename(os.path.dirname(dataset_dir))
+        else:
+            dataset_type = os.path.basename(dataset_dir)
+
     if dataset_type == "collective":
         clip_dirs = sorted(glob(os.path.join(dataset_dir, "*")))
     elif dataset_type == "volleyball":
@@ -36,6 +45,8 @@ def main():
         clip_dirs = []
         for clip_dir in video_dirs:
             clip_dirs += sorted(glob(os.path.join(clip_dir, "*/")))
+    elif dataset_type == "video":
+        raise NotImplementedError
     else:
         raise TypeError
 
@@ -51,7 +62,9 @@ def main():
         for frame_path in tqdm(frame_paths[1:], ncols=100, leave=False):
             frame = cv2.imread(frame_path)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            flow = cv2.calcOpticalFlowFarneback(pre_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            flow = cv2.calcOpticalFlowFarneback(
+                pre_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0
+            )
             flows.append(flow)
             pre_gray = gray
         flows = [np.zeros_like(flow)] + flows
