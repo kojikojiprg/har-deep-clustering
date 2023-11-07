3201442866241d4f67c749b290e6de70cda90cc0
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2023-11-07 20:01:55 +0900

    update tools/opticalflow.py

--- tools/optical_flow.py
+++ tools/optical_flow.py
@@ -1,10 +1,14 @@
 import argparse
 import os
+import sys
 from glob import glob
 
 import cv2
 import numpy as np
 from tqdm import tqdm
+
+sys.path.append("src")
+from utils import video
 
 
 def parser():
@@ -39,41 +43,54 @@ def main():
             dataset_type = os.path.basename(dataset_dir)
 
     if dataset_type == "collective":
-        clip_dirs = sorted(glob(os.path.join(dataset_dir, "*")))
+        clip_dirs = sorted(glob(os.path.join(dataset_dir, "*/")))
     elif dataset_type == "volleyball":
         video_dirs = sorted(glob(os.path.join(dataset_dir, "*/")))
         clip_dirs = []
         for clip_dir in video_dirs:
             clip_dirs += sorted(glob(os.path.join(clip_dir, "*/")))
     elif dataset_type == "video":
-        raise NotImplementedError
+        clip_dirs = sorted(glob(os.path.join(dataset_dir, "*/")))
     else:
         raise TypeError
 
-    for clip_dir in tqdm(clip_dirs, ncols=100):
-        output_path = os.path.join(clip_dir, "flow.npy")
-        if os.path.exists(output_path):
-            continue
-        frame_paths = sorted(glob(os.path.join(clip_dir, "*.jpg")))
+    for clip_dir in tqdm(clip_dirs[:1], ncols=100):
+        # output_path = os.path.join(clip_dir, "flow.npy")
+        # if os.path.exists(output_path):
+        #     continue
 
-        frame = cv2.imread(frame_paths[0])
-        pre_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if dataset_type != "video":
+            frame_paths = sorted(glob(os.path.join(clip_dir, "*.jpg")))
+            frames = [cv2.imread(frame_path) for frame_path in tqdm(frame_paths, leave=False)]
+        else:
+            clip_path = os.path.dirname(clip_dir) + ".mp4"
+            cap = video.Capture(clip_path)
+            # frames = [cap.read()[1] for _ in tqdm(range(cap.frame_count), leave=False)]
+            frames = [cap.read()[1] for _ in tqdm(range(20), leave=False)]
+
+        pre_gray = cv2.cvtColor(frames[0], cv2.COLOR_BGR2GRAY)
         flows = []
-        for frame_path in tqdm(frame_paths[1:], ncols=100, leave=False):
-            frame = cv2.imread(frame_path)
+        for frame in tqdm(frames[1:], ncols=100, leave=False):
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             flow = cv2.calcOpticalFlowFarneback(
                 pre_gray, gray, None, 0.5, 3, 15, 3, 5, 1.2, 0
             )
+            flow = video.flow_to_rgb(flow)
             flows.append(flow)
             pre_gray = gray
         flows = [np.zeros_like(flow)] + flows
 
-        output_path = os.path.join(clip_dir, "flow.npy")
-        np.save(output_path, np.array(flows))
+        # np.save(output_path, np.array(flows))
+
+        output_path = os.path.join(clip_dir, "flow.mp4")
+        wrt = video.Writer(output_path, cap.fps, cap.size)
+        wrt.write_each(flows)
+        del wrt
         tqdm.write(f"saved {output_path}")
 
-        del flows
+        del frames, flows
+        if dataset_type == "video":
+            del cap
 
 
 if __name__ == "__main__":
