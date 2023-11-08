3cc8f1aa00ee0a180e9ce5592a1027761353a582
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2023-11-08 15:35:34 +0900

    update model for lightning 1.9.5

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -26,11 +26,8 @@ class DeepClusteringModel(LightningModule):
         checkpoint_dir: Optional[str] = None,
     ):
         super().__init__()
-        self.automatic_optimization = False
-
         self._cfg = cfg
         self._update_interval = cfg.update_interval
-        self._n_accumulate = cfg.accumulate_grad_batches
         self._n_samples = n_samples
         self._n_samples_batch = n_samples_batch
         self._lmd_cm = cfg.optim.lmd_cm
@@ -41,9 +38,6 @@ class DeepClusteringModel(LightningModule):
 
         self._lr = nn.MSELoss()
         self._lc = nn.KLDivLoss(reduction="sum")
-
-        # self._c_hist_epoch = torch.full((n_samples,), torch.nan)
-        # self._c_hist: list = []  # clustering history
 
         if checkpoint_dir is not None:
             self._callbacks = [
@@ -76,100 +70,38 @@ class DeepClusteringModel(LightningModule):
             [c.detach().numpy() for c in list(iter(self._cm.centroids.cpu()))]
         )
 
-    def training_step(self, batch, batch_idx):
-        frames, flows, bboxs, data_idxs = batch
-        (
-            optim_e_frame,
-            optim_e_flow,
-            optim_d_frame,
-            optim_d_flow,
-            optim_cm,
-        ) = self.optimizers()
-        (
-            sch_e_frame,
-            sch_e_flow,
-            sch_d_frame,
-            sch_d_flow,
-            sch_cm,
-        ) = self.lr_schedulers()
-
-        # train frame autoencoder
-        # self.toggle_optimizer(optim_d_frame)
+    def forward(self, frames, flows, bboxs):
         z_frame, fake_frames = self._ae_frame(frames)
-        lr_frame = self._lr(frames[:, :, -1], fake_frames) / self._n_accumulate
-        self.manual_backward(lr_frame)
-
-        if (batch_idx + 1) % self._n_accumulate == 0:
-            optim_d_frame.step()
-            optim_d_frame.zero_grad()
-            sch_d_frame.step()
-        # self.untoggle_optimizer(optim_d_frame)
-
-        # train flow autoencoder
-        # self.toggle_optimizer(optim_d_flow)
         z_flow, fake_flows = self._ae_flow(flows)
-        lr_flow = self._lr(flows[:, :, -1], fake_flows) / self._n_accumulate
-        self.manual_backward(lr_flow)
-
-        if (batch_idx + 1) % self._n_accumulate == 0:
-            optim_d_flow.step()
-            optim_d_flow.zero_grad()
-            sch_d_flow.step()
-        # self.untoggle_optimizer(optim_d_flow)
-
-        # train clustering
-        # self.toggle_optimizer(optim_cm)
         z = z_frame.detach() + z_flow.detach()
         z, s, c = self._cm(z, bboxs)
 
-        # update clustering label
-        # for i, data_idx in enumerate(data_idxs.cpu()):
-        #     for j in range(self._n_samples_batch):
-        #         idx = data_idx * self._n_samples_batch + j
-        #         self._c_hist_epoch[idx] = c[i, j]
+        return fake_frames, fake_flows, z, s, c
+
+    def training_step(self, batch, batch_idx, optimizer_idx):
+        frames, flows, bboxs, data_idxs = batch
+
+        # forward model
+        fake_frames, fake_flows, z, s, c = self(frames, flows, bboxs)
 
         if (
             self.current_epoch % self._update_interval == 0  # periodical update
             or self.current_epoch == self._cfg.epochs - 1  # last epoch
         ):
-            # save clustering label
-            # self._c_hist.append(self._c_hist_epoch.cpu().detach().numpy())
             # update target
             self._cm.update_target_distribution(s, data_idxs)
 
-        # calc loss clustering
+        # calc autrocoder loss
+        lr_frame = self._lr(frames, fake_frames)
+        lr_flow = self._lr(flows, fake_flows)
+
+        # calc clustering loss
         lc_total = 0
         for i, data_idx in enumerate(data_idxs):
             idx = data_idx * self._n_samples_batch
             tmp_target = self._cm.target_distribution[idx : idx + self._n_samples_batch]
             tmp_s = s[i]
             lc_total = lc_total + self._lc(tmp_s.log(), tmp_target)
-        lc_total = lc_total / self._n_accumulate * self._lmd_cm
-        self.manual_backward(lc_total)
-
-        # step clustering optimizer
-        if self.current_epoch + 1 >= self._cfg.clustering_start_epoch:
-            if (batch_idx + 1) % self._n_accumulate == 0:
-                optim_cm.step()
-                optim_cm.zero_grad()
-                sch_cm.step()
-        # self.untoggle_optimizer(optim_cm)
-
-        # step encoder optimizer
-        # self.toggle_optimizer(optim_e_frame)
-        if (batch_idx + 1) % self._n_accumulate == 0:
-            optim_e_frame.step()
-            optim_e_frame.zero_grad()
-            sch_e_frame.step()
-        # self.untoggle_optimizer(optim_e_frame)
-
-        # step encoder optimizer
-        # self.toggle_optimizer(optim_e_flow)
-        if (batch_idx + 1) % self._n_accumulate == 0:
-            optim_e_flow.step()
-            optim_e_flow.zero_grad()
-            sch_e_flow.step()
-        # self.untoggle_optimizer(optim_e_flow)
 
         self.log_dict(
             {"lr_frame": lr_frame, "lr_flow": lr_flow, "lc": lc_total},
@@ -177,16 +109,33 @@ class DeepClusteringModel(LightningModule):
             on_epoch=True,
         )
 
+        if optimizer_idx == 0:  # frame encoder
+            return lr_frame + lc_total * self._lmd_cm
+        elif optimizer_idx == 1:  # flow encoder
+            return lr_flow + lc_total * self._lmd_cm
+        elif optimizer_idx == 2:  # frame decoder
+            return lr_frame
+        elif optimizer_idx == 3:  # flow decoder
+            return lr_flow
+        elif optimizer_idx == 4:  # clustering
+            # if self.current_epoch + 1 >= self._cfg.clustering_start_epoch:
+            return lc_total
+            # else:
+            #     return None
+        else:
+            raise ValueError(f"optimizer_idx {optimizer_idx}")
+
     def validation_step(self, batch, batch_idx):
         frames, flows, bboxs, data_idxs = batch
 
-        # train autoencoder
+        # forward autoencoder
         _, frames_fake = self._ae_frame(frames)
         _, flows_fake = self._ae_flow(flows)
 
         # save fig
         frames = frames[:, :, -1]
         frames = frames.permute(0, 2, 3, 1).detach().cpu().numpy()
+        frames_fake = frames_fake[:, :, -1]
         frames_fake = frames_fake.permute(0, 2, 3, 1).detach().cpu().numpy()
         frames = ((frames + 1) / 2 * 255).astype(np.uint8)
         frames_fake = ((frames_fake + 1) / 2 * 255).astype(np.uint8)
@@ -194,9 +143,10 @@ class DeepClusteringModel(LightningModule):
         cv2.imwrite(f"images/batch{batch_idx}_frameout.jpg", frames_fake[0])
 
         flow = flows[:, :, -1].permute(0, 2, 3, 1).cpu().numpy()[0]
-        flow = flow_to_rgb(flow, frames.shape[1:])
+        flow = flow_to_rgb(flow)
+        flows_fake = flows_fake[:, :, -1]
         flow_fake = flows_fake.permute(0, 2, 3, 1).cpu().numpy()[0]
-        flow_fake = flow_to_rgb(flow_fake, frames.shape[1:])
+        flow_fake = flow_to_rgb(flow_fake)
         cv2.imwrite(f"images/batch{batch_idx}_flowin.jpg", flow)
         cv2.imwrite(f"images/batch{batch_idx}_flowout.jpg", flow_fake)
 
