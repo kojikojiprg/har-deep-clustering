f1cff63783bcfb85e2f2110513a89e39d627ff42
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2023-12-01 18:34:25 +0900

    update model training step

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -27,6 +27,7 @@ class DeepClusteringModel(LightningModule):
     ):
         super().__init__()
         self._cfg = cfg
+        self._update_interval = cfg.update_interval
         self._n_samples = n_samples
         self._n_samples_batch = n_samples_batch
         self._lmd_cm = cfg.optim.lmd_cm
@@ -79,7 +80,8 @@ class DeepClusteringModel(LightningModule):
 
         if optimizer_idx == 0:  # update target
             # if self.current_epoch + 1 >= self._cfg.clustering_start_epoch:
-            self._cm.update_target_distribution(s, data_idxs)
+            if self.current_epoch % self._update_interval == 0:
+                self._cm.update_target_distribution(s, data_idxs)
 
         # calc autrocoder loss
         if optimizer_idx in [0, 2]:
