a69c96bd8e37b9e8c0ffe60514c851508ca20e6b
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2023-12-15 18:41:49 +0900

    add training_autoencoder

--- src/model/autoencoder.py
+++ src/model/autoencoder.py
@@ -1,6 +1,18 @@
+import sys
+from types import SimpleNamespace
+
+import cv2
+import numpy as np
 import torch.nn as nn
+from lightning.pytorch import LightningModule
+from lightning.pytorch.callbacks import ModelCheckpoint
+from torch.optim import Adam
+from torch.optim.lr_scheduler import StepLR
 
 from .i3d import InceptionI3d
+
+sys.path.append("src")
+from utils.video import flow_to_rgb
 
 
 class Encoder(nn.Module):
@@ -79,3 +91,87 @@ class Autoencoder(nn.Module):
         fake_imgs = self._d(z)
         # fake_imgs(b, c, seq_len, h, w)
         return z, fake_imgs
+
+
+class AutoencoderModule(LightningModule):
+    def __init__(self, cfg: SimpleNamespace, datatype: str, checkpoint_dir: str):
+        super().__init__()
+        self._cfg = cfg
+
+        if datatype == "frame":
+            n_channels = 3
+        elif datatype == "flow":
+            n_channels = 2
+        else:
+            raise ValueError
+        self._datatype = datatype
+
+        self._ae = Autoencoder(cfg, n_channels)
+        self._lr = nn.MSELoss()
+
+        self._callbacks = [
+            ModelCheckpoint(
+                checkpoint_dir,
+                filename=f"ae_{datatype}_seq{cfg.seq_len}_lc_min",
+                monitor="lr",
+                mode="min",
+                save_last=True,
+            ),
+        ]
+        last_name = f"ae_{datatype}_seq{cfg.seq_len}_last"
+        self._callbacks[0].CHECKPOINT_NAME_LAST = last_name
+
+    @property
+    def callbacks(self) -> list:
+        return self._callbacks
+
+    def training_step(self, batch, batch_idx):
+        frames, flows, bboxs, norms, data_idxs = batch
+        if self._datatype == "frame":
+            x = frames
+        elif self._datatype == "flow":
+            x = flows
+
+        _, fakes = self._ae(x)
+        lr = self._lr(x, fakes)
+        self.log("lr", lr, on_epoch=True)
+        return lr
+
+    def validation_step(self, batch, batch_idx):
+        frames, flows, bboxs, norms, data_idxs = batch
+        if self._datatype == "frame":
+            x = frames
+        elif self._datatype == "flow":
+            x = flows
+
+        # forward autoencoder
+        _, fakes = self._ae(x)
+
+        # save fig
+        if self._datatype == "frame":
+            frames = frames[:, :, -1]
+            frames = frames.permute(0, 2, 3, 1).detach().cpu().numpy()
+            fakes = fakes[:, :, -1]
+            fakes = fakes.permute(0, 2, 3, 1).detach().cpu().numpy()
+            frames = ((frames + 1) / 2 * 255).astype(np.uint8)
+            fakes = ((fakes + 1) / 2 * 255).astype(np.uint8)
+            cv2.imwrite(f"images/batch{batch_idx}_framein.jpg", frames[0])
+            cv2.imwrite(f"images/batch{batch_idx}_frameout.jpg", fakes[0])
+
+        elif self._datatype == "flow":
+            flow = flows[:, :, -1].permute(0, 2, 3, 1).cpu().numpy()[0]
+            flow = flow_to_rgb(flow)
+            fakes = fakes[:, :, -1]
+            flow_fake = fakes.permute(0, 2, 3, 1).cpu().numpy()[0]
+            flow_fake = flow_to_rgb(flow_fake)
+            cv2.imwrite(f"images/batch{batch_idx}_flowin.jpg", flow)
+            cv2.imwrite(f"images/batch{batch_idx}_flowout.jpg", flow_fake)
+
+    def configure_optimizers(self):
+        # optimizer
+        optim = Adam(self._ae.parameters(), self._cfg.optim.lr)
+        # scheduler
+        step_size = self._cfg.epochs // 2
+        sch = StepLR(optim, step_size, 0.1)
+
+        return [optim], [sch]
---
+++ tools/training_autoencoder.py
@@ -0,0 +1,88 @@
+import argparse
+import os
+import sys
+
+from lightning.pytorch import Trainer
+from lightning.pytorch.loggers import TensorBoardLogger
+
+sys.path.append("src")
+from dataset import Datamodule
+from model import AutoencoderModule
+from utils import file_io
+
+
+def parser():
+    parser = argparse.ArgumentParser()
+
+    # positional
+    parser.add_argument("dataset_dir", type=str)
+    parser.add_argument("datatype", type=str, help="'frame' or 'flow'")
+
+    # optional
+    parser.add_argument("-dt", "--dataset_type", type=str, required=False, default=None)
+    parser.add_argument(
+        "-mc",
+        "--model_config_path",
+        type=str,
+        required=False,
+        default="configs/model_config_autoencoder.yaml",
+    )
+    parser.add_argument("--checkpoint_dir", type=str, required=False, default="models/")
+    parser.add_argument("--log_dir", type=str, required=False, default="logs/")
+    parser.add_argument(
+        "-g", "--gpus", type=int, nargs="*", default=None, help="gpu ids"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+def main():
+    # get args
+    args = parser()
+    dataset_dir = args.dataset_dir
+    datatype = args.datatype
+    dataset_type = args.dataset_type
+    model_config_path = args.model_config_path
+    checkpoint_dir = args.checkpoint_dir
+    log_dir = args.log_dir
+    gpu_ids = args.gpus
+
+    # get config
+    config = file_io.get_config(model_config_path)
+
+    # create dataset
+    print(f"=> creating dataset from {dataset_dir}")
+    if dataset_type is None:
+        if dataset_dir.endswith("/"):
+            dataset_type = os.path.basename(os.path.dirname(dataset_dir))
+        else:
+            dataset_type = os.path.basename(dataset_dir)
+    datamodule = Datamodule(dataset_dir, dataset_type, config, "train")
+
+    # create model
+    print("=> create model")
+    checkpoint_dir = os.path.join(checkpoint_dir, dataset_type, "autoencoder")
+    model = AutoencoderModule(config, datatype, checkpoint_dir)
+
+    # training
+    log_dir = os.path.join(log_dir, dataset_type, "autoencoder")
+    trainer = Trainer(
+        logger=TensorBoardLogger(log_dir, name=datatype),
+        callbacks=model.callbacks,
+        max_epochs=config.epochs,
+        accumulate_grad_batches=config.accumulate_grad_batches,
+        accelerator="gpu",
+        devices=gpu_ids,
+        strategy="ddp",
+        # strategy=fsdp,
+    )
+    print("=> training")
+    trainer.fit(model, datamodule=datamodule)
+
+    print("=> complete")
+
+
+if __name__ == "__main__":
+    main()
