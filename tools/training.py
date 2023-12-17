a61423b3ee3799849d3621025f8d14f6a793f9b4
Koji Yokoyama <yokoyamak@kuhp.kyoto-u.ac.jp>
2023-12-17 20:58:56 +0900

    update to select model_type

--- tools/training.py
+++ tools/training.py
@@ -9,7 +9,7 @@ from lightning.pytorch.loggers import TensorBoardLogger
 
 sys.path.append("src")
 from dataset import Datamodule
-from model import DeepClusteringModel
+from model import select_deep_clustering_module
 from utils import file_io
 
 
@@ -18,6 +18,7 @@ def parser():
 
     # positional
     parser.add_argument("dataset_dir", type=str)
+    parser.add_argument("model_type", type=str, help="'frame_flow' or 'flow'")
 
     # optional
     parser.add_argument("-dt", "--dataset_type", type=str, required=False, default=None)
@@ -43,6 +44,7 @@ def main():
     # get args
     args = parser()
     dataset_dir = args.dataset_dir
+    model_type = args.model_type
     dataset_type = args.dataset_type
     model_config_path = args.model_config_path
     checkpoint_dir = args.checkpoint_dir
@@ -66,7 +68,8 @@ def main():
     n_samples = datamodule.n_samples
     n_samples_batch = datamodule.n_samples_batch
     checkpoint_dir = os.path.join(checkpoint_dir, dataset_type)
-    model = DeepClusteringModel(
+    model = select_deep_clustering_module(
+        model_type,
         config,
         n_samples,
         n_samples_batch,
@@ -76,8 +79,9 @@ def main():
 
     # training
     # fsdp = FSDPStrategy(cpu_offload=True)
+    log_dir = os.path.join(log_dir, dataset_type)
     trainer = Trainer(
-        logger=TensorBoardLogger(log_dir, name=dataset_type),
+        logger=TensorBoardLogger(log_dir, name=model_type),
         callbacks=model.callbacks,
         max_epochs=config.epochs,
         accumulate_grad_batches=config.accumulate_grad_batches,
